4f16bcb9a5119be4e3aef05fed2f4434910ca398
Vinodambarapu267 <150525184+Vinodambarapu267@users.noreply.github.com>
2024-12-07 14:55:14 +0530

    feat:implements DishRepository

--- src/com/javaProject/foodiecli/repository/DishRepository.java
+++ src/com/javaProject/foodiecli/repository/DishRepository.java
@@ -4,6 +4,7 @@ import com.javaProject.foodiecli.Model.Dish;
 import com.javaProject.foodiecli.util.CsvReader;
 
 import java.util.List;
+import java.util.Optional;
 
 public class DishRepository {
     List<Dish> dishList;
@@ -20,5 +21,19 @@ public class DishRepository {
     public Dish saveDish(Dish dish) {
         this.dishList.add(dish);
         return dish;
+    }
+    public Dish updateDish(Dish dishToBeUpdated) throws DishNotFoundException{
+        Optional<Dish> optionalDish = this.dishList.stream().filter(dish -> dish.getDishId().equals(dishToBeUpdated.getDishId()))
+                .findFirst()
+                .map(Dish -> {
+                    Dish.setDishName(dishToBeUpdated.getDishName())
+                            .setDescription(dishToBeUpdated.getDescription())
+                            .setPrice(dishToBeUpdated.getPrice());
+                    return Dish;
+                });
+        return optionalDish.orElse(null);
+    }
+    public void deleteDish(Dish dish){
+        this.dishList.remove(dish);
     }
 }
