2486c034e2ef12cd725f183e0af65a2c5ced8678
Vinodambarapu267 <150525184+Vinodambarapu267@users.noreply.github.com>
2024-12-07 15:12:00 +0530

    feat:Implemented CRUD Methods

--- src/com/javaProject/foodiecli/repository/CustomerRepository.java
+++ src/com/javaProject/foodiecli/repository/CustomerRepository.java
@@ -7,22 +7,40 @@ import java.util.List;
 import java.util.Optional;
 
 public class CustomerRepository {
-   private List<Customer> customersList;
+    List<Customer> customerList;
 
-    public CustomerRepository() {
-        CsvReader csvReader = new CsvReader();
-        this.customersList =csvReader.readCustomersFromCsv();
+    public CustomerRepository(){
+        this.customerList= Factory.getCsvReader().readCustomerFromCsv();
     }
 
-    public List<Customer> getAllCustomers() {
-        return this.customersList;
+    public List<Customer> getCustomerList(){
+        return this.customerList;
     }
 
-    public Customer save(Customer customer) {
-        this.customersList.add(customer);
+    public Customer saveCustomer(Customer customer){
+        this.customerList.add(customer);
         return customer;
     }
-    public  Optional<Customer> findCustomerById(String id){
-         return  this.customersList.stream().filter(customer -> customer.getCustomerId().equals(id)).findFirst();
+    public Optional<Customer> getCustomerById(String Id){
+        return  this.customerList.stream().filter(customer -> customer.getCustomerId().equals(Id)).findFirst();
     }
+    public Optional<Customer> getCustomerByEmailAndPassword(String Email,String passWord){
+        return  this.customerList.stream().filter(customer -> customer.getEmail().equals(Email) && customer.getPassword().equals(passWord)).findFirst();
+    }
+    public Customer updateCustomer(Customer customerToBeUpdated){
+        Optional<Customer> customerOptional = this.customerList.stream().filter(customer -> customer.getCustomerId().equals(customerToBeUpdated.getCustomerId()))
+                .findFirst()
+                .map(customer ->
+                {
+                    customer.setCustomerName(customerToBeUpdated.getCustomerName())
+                            .setEmail(customerToBeUpdated.getEmail())
+                            .setPassword(customerToBeUpdated.getPassword());
+                    return customer;
+                });
+        return customerOptional.orElse(null);
+    }
+    public void deleteCustomer(Customer customer){
+        this.customerList.remove(customer);
+    }
+
 }
