0ad4a7fb2744e736904bbbf6e3be9af732ba2eed
Vinodambarapu267 <150525184+Vinodambarapu267@users.noreply.github.com>
2024-12-07 16:29:55 +0530

    feat:update Functionality

--- src/com/javaProject/foodiecli/Controller/CustomerController.java
+++ src/com/javaProject/foodiecli/Controller/CustomerController.java
@@ -2,15 +2,46 @@ package com.javaProject.foodiecli.Controller;
 
 import com.javaProject.foodiecli.Model.Customer;
 import com.javaProject.foodiecli.exceptions.CustomerExistsException;
+import com.javaProject.foodiecli.exceptions.CustomerNotFoundException;
 import com.javaProject.foodiecli.service.CustomerService;
+import com.javaProject.foodiecli.service.CustomerServiceImpl;
+
+import java.util.List;
 
 public class CustomerController {
-    private CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
+    private final CustomerServiceImpl customerService;
+
+    public CustomerController(CustomerServiceImpl customerService) {
         this.customerService = customerService;
     }
+
     public Customer save(Customer customer) throws CustomerExistsException {
-        return  this.customerService.save(customer);
+        return this.customerService.save(customer);
+    }
+
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotFoundException {
+        Customer customer = this.customerService.validateCustomerLogin(email, password);
+        if(customer != null)
+            this.customerService.setCurrentLoggedInCustomer(customer);
+        return customer;
+    }
+
+    public Customer currentLoggedInCustomer(){
+        return this.customerService.getCurrentLoggedInCustomer();
+    }
+
+    public Customer getCustomerById(String id) throws CustomerNotFoundException{
+        return this.customerService.getCustomerById(id);
+    }
+
+    public List<Customer> getCustomersList(){ return this.customerService.getAllCustomers();}
+
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+        return this.customerService.updateCustomer(customer);
+    }
+
+    public void deleteCustomer(String id) throws CustomerNotFoundException{
+        this.customerService.deleteCustomer(id);
     }
 }
